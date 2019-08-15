60009fea69cddf4fa98eb0eb178f434826ac9208
Wasselin Romain <IN-BR-006@192.168.1.92>
2019-08-15 18:46:36 +0200

    Ajout de la classe entité Client

---
+++ 12_ProjetECommerce/src/fr/adaming/entity/Client.java
@@ -0,0 +1,11 @@
+package fr.adaming.entity;
+
+public class Client {
+	
+	private Long idClient;
+	private String nomClient;
+	private String adresse;
+	private String mail;
+	private String telephone;
+
+}
