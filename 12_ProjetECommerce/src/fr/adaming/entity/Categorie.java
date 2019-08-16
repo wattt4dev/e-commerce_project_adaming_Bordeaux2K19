5d4bf0cdbc3fa2f79d0be4c974d89f698df56400
audrey.dede <aarnould00@gmail.com>
2019-08-16 10:49:40 +0200

    ajout entity (ligne de commande et categorie)

---
+++ 12_ProjetECommerce/src/fr/adaming/entity/Categorie.java
@@ -0,0 +1,75 @@
+package fr.adaming.entity;
+
+public class Categorie {
+	
+	
+	//declaration des attributs
+	private long idCategorie;
+	private String nomCategorie;
+	private byte[] photo;
+	private String description;
+	
+	
+	
+	//constructeurs (vide et chargé)
+	
+	public Categorie() {
+		super();
+	}
+
+	public Categorie(String nomCategorie, byte[] photo, String description) {
+		super();
+		this.nomCategorie = nomCategorie;
+		this.photo = photo;
+		this.description = description;
+	}
+
+	public Categorie(long idCategorie, String nomCategorie, byte[] photo, String description) {
+		super();
+		this.idCategorie = idCategorie;
+		this.nomCategorie = nomCategorie;
+		this.photo = photo;
+		this.description = description;
+	}
+
+
+	
+	//getters et setters 
+	
+	public long getIdCategorie() {
+		return idCategorie;
+	}
+
+	public void setIdCategorie(long idCategorie) {
+		this.idCategorie = idCategorie;
+	}
+
+	public String getNomCategorie() {
+		return nomCategorie;
+	}
+
+	public void setNomCategorie(String nomCategorie) {
+		this.nomCategorie = nomCategorie;
+	}
+
+	public byte[] getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
+	
+
+}
---
+++ 12_ProjetECommerce/src/fr/adaming/entity/LigneCommande.java
@@ -0,0 +1,52 @@
+package fr.adaming.entity;
+
+public class LigneCommande {
+	
+	
+	//déclaration des attributs
+	private int quantité;
+	private double prix;
+
+	
+	
+	//constructeurs (vide et chargés)
+	public LigneCommande() {
+		super();
+	}
+
+
+
+	public LigneCommande(int quantité, double prix) {
+		super();
+		this.quantité = quantité;
+		this.prix = prix;
+	}
+
+
+	//getters et setters 
+
+	public int getQuantité() {
+		return quantité;
+	}
+
+
+	public void setQuantité(int quantité) {
+		this.quantité = quantité;
+	}
+
+
+	public double getPrix() {
+		return prix;
+	}
+
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+	
+	
+	
+	
+	
+
+}
