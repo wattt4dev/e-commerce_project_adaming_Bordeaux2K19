31e82275fc1f7995e6e5b7e4fde64f41554fab51
Wasselin Romain <IN-BR-006@192.168.1.92>
2019-08-18 15:26:16 +0200

    Modif AppTestCategorie

--- 12_ProjetECommerce/src/fr/adaming/test/AppTestCategorie.java
+++ 12_ProjetECommerce/src/fr/adaming/test/AppTestCategorie.java
@@ -2,10 +2,11 @@ package fr.adaming.test;
 
 import org.omg.CORBA.PRIVATE_MEMBER;
 
+import fr.adaming.entity.Categorie;
 import fr.adaming.service.BoutiqueServiceImpl;
 import fr.adaming.service.IAdminCategorieService;
 
-public class AppTest {
+public class AppTestCategorie {
 	
 	public static void main(String[] args) {
 		
@@ -15,11 +16,12 @@ public class AppTest {
 
 			//Déclaration du Service Categorie
 		
-		private IAdminCategorieService categorieService = new BoutiqueServiceImpl();
+		IAdminCategorieService categorieService = new BoutiqueServiceImpl();
 		
 			//Ajouter une catégorie
-		
-		
+		byte[] photo = {15,12,14};
+		Categorie categorie = new Categorie("Appareil Photo", photo, "Appareil Photo numérique");
+		categorieService.ajouterCategorie(categorie);
 		
 		
 		
